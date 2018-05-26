64cf39c3f4de7bfa02d08ef5ffc1be9c60818560
Zefevr <zefevillalba@gmail.com>
2018-05-26 14:21:41 +0200

    post endpoint working but need to look into the Json

--- src/game/controller.ts
+++ src/game/controller.ts
@@ -1,13 +1,22 @@
-import { JsonController, Get, Param } from 'routing-controllers'
+import { JsonController, Get, Param, Post, HttpCode, BodyParam } from 'routing-controllers'
 import Game from './entity'
 
 //type GameList = { pages: Game[] }
+
+const defaultBoard = [
+	['o', 'o', 'o'],
+	['o', 'o', 'o'],
+	['o', 'o', 'o']
+]
+
+const randomcolor=['red','blue','green','yellow','magenta']
+
 
 @JsonController()
 export default class GameController {
 
     @Get('/games/:id')
-    getPage(
+    getGame(
     @Param('id') id: number
     ) {
     return Game.findOne(id)
@@ -18,7 +27,21 @@ export default class GameController {
     const games = Game.find()
     return { games }
     }
+    
+
+    @Post('/games')
+    @HttpCode(201)
+    createGame(
+    @BodyParam("name") name : string
+    ){
+    const game = new Game()
+    game.name = name
+    game.color = randomcolor[Math.floor(Math.random() * randomcolor.length)]
+    game.board = JSON.stringify(defaultBoard)
+    return game.save()
     }
+
+}
 /* 
     @Put('/pages/:id')
     updatePage(
